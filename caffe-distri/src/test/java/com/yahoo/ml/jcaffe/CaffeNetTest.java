5b3b66a2a457ae4517a306cdac17e492d71a2c10
Shiqing Fan <sqfan6@gmail.com>
2016-08-19 16:23:01 +0800

    Add caffeOnSpark_ps test

--- caffe-distri/src/test/java/com/yahoo/ml/jcaffe/CaffeNetTest.java
+++ caffe-distri/src/test/java/com/yahoo/ml/jcaffe/CaffeNetTest.java
@@ -9,10 +9,14 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.google.protobuf.TextFormat;
 
+import org.parameterserver.client.PSClient;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,11 +29,13 @@ public class CaffeNetTest {
     int index = 0;
     List<String> file_list;
 
-    final int batchs = 20;
     final int batch_size = 5;
     final int channels = 3;
     final int height = 227;
     final int width = 227;
+
+    String psMasterAddr = "localhost:60987"; //TODO: To check that PS is running
+    String weightVector = null;
 
     @BeforeMethod
     public void setUp() throws Exception {
@@ -139,7 +145,123 @@ public class CaffeNetTest {
     }
 
     @Test
+    public void testMultiNetTrainWithPS() throws Exception{
+        final AtomicInteger counter = new AtomicInteger(0);
+        class netThread implements Runnable {
+            private CaffeNet net;
+            public netThread(CaffeNet net) {
+                this.net = net;
+            }
+            @Override
+            public void run() {
+                try {
+                    nStepsTrain(3, this.net, null, true);
+                    counter.incrementAndGet();
+                } catch (Exception ioe) {
+                    ioe.printStackTrace();
+                }
+            } // end of run
+        }
+        this.weightVector = "w-" + (new Random()).nextLong();
+        int cluster_size = 3;
+        PSClient psClient = new PSClient(psMasterAddr);
+        psClient.setContext("Caffe_On_Spark_PS_" + weightVector);
+        psClient.bspInitializeContext(cluster_size);
+
+        CaffeNet net1 = new CaffeNet(solver_config_path, "", "", 1, cluster_size, 0, true,
+          CaffeNet.SOCKET, -1, psMasterAddr, weightVector);
+        CaffeNet net2 = new CaffeNet(solver_config_path, "", "", 1, cluster_size, 1, true,
+          CaffeNet.SOCKET, -1, psMasterAddr, weightVector);
+        CaffeNet net3 = new CaffeNet(solver_config_path, "", "", 1, cluster_size, 2, true,
+          CaffeNet.SOCKET, -1, psMasterAddr, weightVector);
+
+        assertTrue(net1 != null && net2 != null && net3 != null);
+//        assertTrue(net1 != null && net2 != null);
+
+        Thread t1 = new Thread(new netThread(net1));
+        Thread t2 = new Thread(new netThread(net2));
+        Thread t3 = new Thread(new netThread(net3));
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        assertEquals(counter.get(), cluster_size);
+
+//        net1.deallocate();
+//        net2.deallocate();
+//        net3.deallocate();
+    }
+
+    @Test
+    public void getSetWeightJNITest() throws Exception {
+        float[] weights = net.getLocalWeights();
+
+        // JNI setLocalWeights test
+        float[] newWeights = randomFloatArray(weights.length);
+        assertTrue(net.setLocalWeights(newWeights));
+        weights = net.getLocalWeights(); // re-fetch local weights
+        assertEquals(weights, newWeights);
+
+        // One-step training, the caffe local weights should have been changed
+        nStepsTrain(1, net, null, false);
+
+        assertNotEquals(weights, net.getLocalWeights());
+    }
+
+    @Test
+    public void getGradientJNITest() throws Exception {
+        double momentum = 0.9;
+        double base_lr = 0.001;
+        // Get weights from local caffe before training
+        float[] weights = new float[net.getLocalWeights().length];
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = (float) 1.0;
+        }
+        net.setLocalWeights(weights);
+
+        float[] gradients, gradients2;
+
+        // Get gradients from local caffe test
+        gradients = net.getLocalGradients();
+
+        // One-step training from initial weights and re-fetch local gradients
+        nStepsTrain(1, net, null, false);
+
+        gradients2 = net.getLocalGradients();
+        float[] newWeights = net.getLocalWeights();
+
+        float[] expectedNewWeight = new float[weights.length];
+        for (int i = 0; i < weights.length; i++) {
+            expectedNewWeight[i] = (float) (weights[i] + (momentum * gradients[i]
+              - base_lr * gradients2[i]));
+        }
+
+        assertEquals(gradients.length, gradients2.length);
+        // TODO: how to test gradients? (@shiqing) Then comment me back
+//        org.junit.Assert.assertArrayEquals(expectedNewWeight, newWeights, (float) 1e-5);
+        assertEquals(weights.length, gradients.length);
+    }
+
+    @Test
     public void testTrain() throws Exception {
+        int batchs = 5;
+        nStepsTrain(batchs, net, test_net, false);
+    }
+
+  /**
+   * Do # steps of local training
+   *
+   * @param steps # steps of local training
+   * @param trainNet caffeNet instance for interacting with Caffe (c++)
+   * @param testNet if not null, also do # steps of simplified caffenet testing
+   * @param withPS if true, use parameter server for parameter synchronization
+   * @throws Exception
+   */
+    private void nStepsTrain(int steps, CaffeNet trainNet, CaffeNet testNet, boolean withPS) throws Exception {
         SolverParameter solver_param = Utils.GetSolverParam(rootPath + "caffe-distri/src/test/resources/caffenet_solver.prototxt");
 
         String net_proto_file = solver_param.getNet();
@@ -160,43 +282,35 @@ public class CaffeNetTest {
         TransformationParameter param = train_layer_param.getTransformParam();
         FloatDataTransformer xform = new FloatDataTransformer(param, true);
 
-        // Get weights from local caffe
-        float[] weights = net.getLocalWeights();
-        float[] gradients;
-
         //simplified training
         System.out.print("CaffeNetTest training:");
-        for (int i=0; i<batchs; i++) {
+        for (int i = 0; i < steps; i++) {
             System.out.print(".");
             nextBatch(matVec, labelblob);
             xform.transform(matVec, data_blob);
-            assertTrue(net.train(0, dataBlobs, labelblob.cpu_data()));
-
-            // Get gradients from local caffe test
-            gradients = net.getLocalGradients();
-            assertEquals(weights.length, gradients.length);
+            if (withPS)
+                assertTrue(trainNet.trainWithPS(0, dataBlobs, labelblob.cpu_data()));
+            else
+                assertTrue(trainNet.train(0, dataBlobs, labelblob.cpu_data()));
         }
 
-        //simplified test
-        String[] test_features = {"loss"};
-        System.out.print("CaffeNetTest test:");
-        for (int i=0; i<batchs; i++) {
-            System.out.print(".");
-            nextBatch(matVec, labelblob);
-            xform.transform(matVec, data_blob);
-            FloatBlob[] top_blobs_vec = net.predict(0, dataBlobs, labelblob.cpu_data(), test_features);
-            //validate test results
-            for (int j = 0; j< top_blobs_vec.length; j++) {
-                FloatArray result_vec = top_blobs_vec[j].cpu_data();
-                assertTrue(result_vec.get(0) < 50.0);
+        if (testNet != null) {
+            //simplified test
+            String[] test_features = {"loss"};
+            System.out.print("CaffeNetTest test:");
+            for (int i = 0; i < steps; i++) {
+                System.out.print(".");
+                nextBatch(matVec, labelblob);
+                xform.transform(matVec, data_blob);
+                FloatBlob[] top_blobs_vec =
+                  testNet.predict(0, dataBlobs, labelblob.cpu_data(), test_features);
+                //validate test results
+                for (int j = 0; j < top_blobs_vec.length; j++) {
+                    FloatArray result_vec = top_blobs_vec[j].cpu_data();
+                    assertTrue(result_vec.get(0) < 50.0);
+                }
             }
         }
-
-        // JNI setLocalWeights test
-        float[] newWeights = randomFloatArray(weights.length);
-        assertTrue(net.setLocalWeights(newWeights));
-        weights = net.getLocalWeights(); // re-fetch local weights
-        assertEquals(weights, newWeights);
 
         //release C++ resource
         xform.deallocate();
